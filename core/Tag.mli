9aebd8a9c99df497ea1c4540d7442c46e9f9cdf5
Martin Jambon <martin@mjambon.com>
2024-02-19 22:03:38 -0800

    Format and improve reference documentation

--- core/Tag.mli
+++ core/Tag.mli
@@ -1,16 +1,38 @@
-(*
-   Test tags
+(**
+   {0 Test tags}
 
-   Tags are strings which is nice and extensible, but to prevent
+   A tag contributes to describing what a test is about in addition to
+   the test's name and category. Additionally, it allows for the precise
+   selection of a group of tests via the command line.
+*)
+
+(**
+   Tags are strings. This is nice and extensible, but to prevent
    misspellings and conflicts, we require them to be registered
-   using 'Tag.declare'.
+   using {!declare}.
 *)
 type t = private string
 
-(* Create a tag. Each tag may only be created once. *)
+(** Create and register a tag. This function raises exceptions if used
+    improperly.
+
+    A tag must be created exactly once. It must be a non-empty dot-separated
+    sequence of lowercase alphanumeric identifiers ([[a-z_][a-z_0-9]*]).
+*)
 val declare : string -> t
+
+(** List all the registered tags. *)
 val list : unit -> t list
+
+(** Compare two tags in ASCII (alphabetic) order. *)
 val compare : t -> t -> int
+
+(** Determine if two tags are equal. *)
 val equal : t -> t -> bool
-val show : t -> string
+
+(** Convert the tag back to the original string. *)
 val to_string : t -> string
+
+(** Convert the tag to human-readable form (which happens to be the same as
+    the original string). *)
+val show : t -> string
