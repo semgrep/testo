0fea0ec418be27469cc834bdfa8075e03f2ea973
Martin Jambon <martin@mjambon.com>
2024-03-12 19:53:10 -0700

    Fix documentation

--- core/Testo.mli
+++ core/Testo.mli
@@ -327,11 +327,11 @@ val mask_pcre_pattern : ?mask:string -> string -> (string -> string)
    {- [depth]: maximum number of path segments to mask after [/tmp] or
                equivalent.
                For example, [/tmp/b4ac9882/foo/bar] will become
-               [<MASKED TEMPORARY PATH>foo/bar] with the default depth
+               [<TEMPORARY FILE PATH>foo/bar] with the default depth
                of [Some 1]. With a depth of 2, if would become
-               [<MASKED TEMPORARY PATH>bar]}. Use [None] to mask the full
+               [<TEMPORARY FILE PATH>bar]}. Use [None] to mask the full
                path. Use [Some 0] to mask only [/tmp] or equivalent.
-   {- [mask]: replacement string. Defaults to [<MASKED TEMPORARY PATH>].}
+   {- [mask]: replacement string. Defaults to [<TEMPORARY FILE PATH>].}
 }
 *)
 val mask_temp_paths :
