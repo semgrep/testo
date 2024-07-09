4f457e05c702a6bf248ded0e9b3aaebed5b74b65
Martin Jambon <martin@mjambon.com>
2024-07-08 19:23:07 -0700

    Use better test names

--- tests/Parallel_test.ml
+++ tests/Parallel_test.ml
@@ -2,19 +2,15 @@
    Simple test suite with minimal output, used to test parallel workers
 *)
 
-let t = Testo.create
-let test_sleep () = Unix.sleepf 0.05
+open Printf
+
+let sleep_duration = 0.05
+let test_sleep () = Unix.sleepf sleep_duration
 
 let tests _env =
-  [
-    t "test sleep #1" test_sleep;
-    t "test sleep #2" test_sleep;
-    t "test sleep #3" test_sleep;
-    t "test sleep #4" test_sleep;
-    t "test sleep #5" test_sleep;
-    t "test sleep #6" test_sleep;
-    t "test sleep #7" test_sleep;
-    t "test sleep #8" test_sleep;
-  ]
+  List.init 8 (fun i ->
+      Testo.create
+        (sprintf "sleep %.0fms #%i" (sleep_duration *. 1000.) (i + 1))
+        test_sleep)
 
 let () = Testo.interpret_argv ~project_name:"testo_parallel_test" tests
